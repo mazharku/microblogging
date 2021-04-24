35f3ddbcb45bf6ba91a4123be0197917cf92412c
mazharku <mazhar.shapnil@gmail.com>
2021-04-24 15:02:26 +0600

    add to log to vote resorce

--- src/main/java/com/mazhar/microblog/repository/VoteRepository.java
+++ src/main/java/com/mazhar/microblog/repository/VoteRepository.java
@@ -17,17 +17,17 @@ import java.util.UUID;
  */
 public interface VoteRepository extends JpaRepository<Vote, UUID>{
 	
-	@Query(value = "SELECT COUNT(vote_id) FROM vote WHERE post_id =:post_id AND vote=true", nativeQuery = true)
+	@Query(value = "SELECT COUNT(*) FROM {h-schema}vote WHERE post_id =:post_id AND vote=true", nativeQuery = true)
 	public long getVotesOfPost(@Param("post_id") UUID postId);
 	
-	@Query(value = "SELECT * FROM vote WHERE voter_id =:voter_id AND post_id=:post_id", nativeQuery = true)
+	@Query(value = "SELECT * FROM {h-schema}vote WHERE voter_id =:voter_id AND post_id=:post_id", nativeQuery = true)
 	public Vote getVoteOfUser(@Param("post_id") UUID post,@Param("voter_id") UUID voter);
 
-	@Query(value = "UPDATE vote  SET vote = :vote  WHERE voter_id = :voter_id AND post_id = :post_id", nativeQuery = true)
+	@Query(value = "UPDATE {h-schema}vote  SET vote = :vote  WHERE voter_id = :voter_id AND post_id = :post_id", nativeQuery = true)
 	public boolean updateVote(@Param("vote") boolean vote,@Param("voter_id") UUID voterId, @Param("post_id") UUID postId);
 
 
-	@Query(value = "SELECT * FROM vote WHERE post_id =:post_id AND vote=true", nativeQuery = true)
+	@Query(value = "SELECT * FROM {h-schema}vote WHERE post_id =:post_id AND vote=true", nativeQuery = true)
 	public List<Vote> getVotesByPost( @Param("post_id") UUID postId);
 
 }
